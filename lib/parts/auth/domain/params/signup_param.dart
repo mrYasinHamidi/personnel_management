279c60bf477864fc8675c0ae64f3c8a3586610b3
yas <yas@gmail.com>
2024-08-21 20:46:32 +0330

    update gitignore

--- lib/parts/auth/domain/params/signup_param.dart
+++ lib/parts/auth/domain/params/signup_param.dart
@@ -4,12 +4,16 @@ part 'signup_param.g.dart';
 
 @JsonSerializable()
 class SignupParam {
+  final String name;
   final String email;
+  final String phone;
   final String password;
 
   const SignupParam({
     required this.email,
     required this.password,
+    required this.name,
+    required this.phone,
   });
 
   factory SignupParam.fromJson(Map<String, dynamic> json) => _$SignupParamFromJson(json);
--- lib/parts/auth/domain/params/signup_param.g.dart
+++ lib/parts/auth/domain/params/signup_param.g.dart
@@ -9,10 +9,14 @@ part of 'signup_param.dart';
 SignupParam _$SignupParamFromJson(Map<String, dynamic> json) => SignupParam(
       email: json['email'] as String,
       password: json['password'] as String,
+      name: json['name'] as String,
+      phone: json['phone'] as String,
     );
 
 Map<String, dynamic> _$SignupParamToJson(SignupParam instance) =>
     <String, dynamic>{
+      'name': instance.name,
       'email': instance.email,
+      'phone': instance.phone,
       'password': instance.password,
     };
